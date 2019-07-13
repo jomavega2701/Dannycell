6b1574138a39a581fc7074d9883335502c984928
jomavega <jomabv79@gmail.com>
2019-07-13 01:51:33 -0500

    creacion tabla usaurios

--- database/migrations/2019_07_13_064643_crear_tabla_usuario.php
+++ database/migrations/2019_07_13_064643_crear_tabla_usuario.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CrearTablaUsuario extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usuario', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('nombre',50);
+            $table->string('apellido',50);
+            $table->string('identificacion',20);
+            $table->string('telefono',20);
+            $table->string('usuario',50);
+            $table->string('contrasena',50);
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuario');
     }
 }
